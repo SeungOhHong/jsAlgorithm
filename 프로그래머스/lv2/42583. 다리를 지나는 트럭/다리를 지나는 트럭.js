2b00edee1616b2ee8c8b678aa4902641cf2cbf8f
SeungOhHong <121365739+SeungOhHong@users.noreply.github.com>
2023-01-12 09:37:24 +0900

    Update 다리를 지나는 트럭.js

--- 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -1,18 +1,18 @@
 function solution(bridge_length, weight, truck_weights) {
     let truck = truck_weights.slice();
     let window = Array(bridge_length).fill(0);
-    let currentWeight = 0;
     let answer = 0;
     
     while (truck.length > 0) {
-        currentWeight -= window.shift();
-        if (currentWeight + truck[0] <= weight) {
-            currentWeight += truck.shift();
-            window.push(1);
+        window.shift();
+        if (window.reduce((a, b) => a + b, 0) + truck[0] <= weight) {
+            window.push(truck.shift());
         } else {
             window.push(0);
         }
         answer++;
     }
+    
     return answer + window.length;
 }
+
